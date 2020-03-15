5376b33262233156dca3fdcc7fa4ff2d0c4ccdfc
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-03-15 14:59:52 -0400

    not yet

--- covid19/scripts/AppleSearch4.js
+++ covid19/scripts/AppleSearch4.js
@@ -1,35 +1,35 @@
  
 // //search function.
-//   $(document).ready(function ()  
-//             {
-//                     $.ajax
-//                         ({
-//                     method: "GET",
-//                     url: "https://aaronlilly.github.io/Coronavirus/covid19/apple4.json"
-//                          }).done(function(data) 
+  $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: "https://aaronlilly.github.io/Coronavirus/covid19/apple4.json"
+                         }).done(function(data) 
 
-//                                {$(document).ready(function () {  
-//         $('#dynam-now').click(function () {
-//          let searchString = $('#dynamId').val();
+                               {$(document).ready(function () {  
+        $('#dynam-now').click(function () {
+         let searchString = $('#dynamId').val();
 
-// let result = _.filter(data.results, function(object) {
+let result = _.filter(data.results, function(object) {
 
-// return object.fruitname.toLowerCase().indexOf(searchString.toLowerCase()) != -1;
+return object.fruitname.toLowerCase().indexOf(searchString.toLowerCase()) != -1;
 
-// });
-//         //console.log(result[0].fruit)
-//         $('#ArrayD').html("");
+});
+        //console.log(result[0].fruit)
+        $('#ArrayD').html("");
  
-// for (var i = 0; i < result.length; i++) {
-//             $('#ArrayD').append(result[i].fruitname + " " + result[i].size + "  " + result[i].color + "<br>")
-//         };
-//     });
-// });   
-//                                 //console.log(data)
+for (var i = 0; i < result.length; i++) {
+            $('#ArrayD').append(result[i].fruitname + " " + result[i].size + "  " + result[i].color + "<br>")
+        };
+    });
+});   
+                                //console.log(data)
                        
                                
-//                               });
-//             });
+                              });
+            });
 //end search function
 
 //onload function
@@ -42,8 +42,13 @@
                          }).done(function(data) 
 
                                {$(document).ready(function () {  
-                                 //$('#ArrayD').append(data.results[0].fruit);
-                                 console.log(data[0].country);
+
+                                for (var i = 0; i < data.length; i++) {
+                                 $('#ArrayD').append(data[i].country + " ");
+
+                               } 
+                                 //console.log(data[0].country);
+
 
           })
 
@@ -54,34 +59,28 @@
 
 
 
-//  $(document).ready(function ()  
-//             {
-//                      {$(document).ready(function () {  
-//                                  $('#ArrayD').append("hi")
 
-//           })
-// };
-//                          });
+//onload function
+  $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: "https://corona.lmao.ninja/countries"
+                         }).done(function(data) 
 
+                               {$(document).ready(function () {  
 
-//
-//   $(document).ready(function ()  
-//             {
-//                     $.ajax
-//                         ({
-//                     method: "GET",
-//                     url: "https://aaronlilly.github.io/Coronavirus/covid19/apple4.json"
-//                          }).done(function(data) 
+                                for (var i = 0; i < data.length; i++) {
+                                 $('#ArrayD').append(data[i].country + " ");
 
-//                                {$(document).ready(function () {  
-//                                  $('#ArrayD').append("hi")
-
-//           })
-// };
- //                        });
-//
+                               } 
+                                 //console.log(data[0].country);
 
 
-// for (var i = 0; i < results.length; i++) {
-//             $('#ArrayD').append(results[i].fruitname + " " + results[i].size + "  " + results[i].color + "<br>")
-//        
+          })
+
+                        });
+                           });
+
+//end onload function

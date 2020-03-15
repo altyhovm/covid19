1ca4533a6167ef4a80ff78fa9fd4a17cfa29b2b1
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-03-15 12:10:24 -0400

    copy

---
+++ covid19/scripts/AppleSearch4.js
@@ -0,0 +1,31 @@
+  $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: "https://aaronlilly.github.io/Coronavirus/externalJsExample/apple4.json"
+                         }).done(function(data) 
+
+                               {$(document).ready(function () {  
+        $('#dynam-now').click(function () {
+         let searchString = $('#dynamId').val();
+
+let result = _.filter(data.results, function(object) {
+
+return object.fruitname.toLowerCase().indexOf(searchString.toLowerCase()) != -1;
+
+});
+        //console.log(result[0].fruit)
+        $('#ArrayD').html("");
+ 
+for (var i = 0; i < result.length; i++) {
+            $('#ArrayD').append(result[i].fruitname + " " + result[i].size + "  " + result[i].color + "<br>")
+        };
+    });
+});   
+                                //console.log(data)
+                       
+                               
+                              });
+            });
+                    
